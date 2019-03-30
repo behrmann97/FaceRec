1f28632e3ad4c7cdde48ab0c0ae8502db7ef89b1
behrmann97 <sbehrmann@uc.cl>
2019-03-30 12:46:51 -0300

    cambios

--- src/glasses_and_stash.py
+++ src/glasses_and_stash.py
@@ -14,12 +14,6 @@ eyes_cascade        = cv2.CascadeClassifier('cascades/third-party/frontalEyes35x
 glasses             = cv2.imread("images/fun/glasses.png", -1)
 hat                 = cv2.imread('images/fun/hat.png',-1)
 
-'''
-OpenCV & Python Tutorial Video Series: https://kirr.co/ijcr59
-Eyes Cascade (and others): https://kirr.co/694cu1
-Nose Cascade / Mustache Post: https://kirr.co/69c1le
-'''
-
 eyes_prev = []
 faces_prev = []
 while(True):
@@ -27,7 +21,10 @@ while(True):
     ret, frame = cap.read()
     gray            = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces           = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    frame = np.zeros((len(frame), len(frame[0]),4), np.uint8)
+    #frame = np.zeros((len(frame), len(frame[0]),4), np.uint8)
+
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
 
     for (x, y, w, h) in faces if faces != () else faces_prev:
@@ -36,7 +33,7 @@ while(True):
 
             roi_color_hat = frame[max(0, y -200):y+h, x:x+w]
 
-            # eyes = eyes_cascade.detectMultiScale(roi_gray, scaleFactor=1.5, minNeighbors=5)
+            eyes = eyes_cascade.detectMultiScale(roi_gray, scaleFactor=1.5, minNeighbors=5)
 
             faces_prev = faces if faces != () else faces_prev
 
@@ -51,20 +48,20 @@ while(True):
             except IndexError:
                 pass
 
-            # try:
-            #
-            #     for (ex, ey, ew, eh) in eyes if eyes != () else eyes_prev:
-            #         #cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
-            #         glasses2 = image_resize(glasses.copy(), width=eh + 20)
-            #         eyes_prev = eyes if eyes != () else eyes_prev
-            #         gw, gh, gc = glasses2.shape
-            #         for i in range(0, gw):
-            #             for j in range(0, gh):
-            #                 #print(glasses[i, j]) #RGBA
-            #                 if glasses2[i, j][3] != 0: # alpha 0
-            #                     roi_color[ey + i , ex + j ] = glasses2[i, j]
-            # except IndexError:
-            #     pass
+            try:
+
+                for (ex, ey, ew, eh) in eyes if eyes != () else eyes_prev:
+                    #cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
+                    glasses2 = image_resize(glasses.copy(), width=eh)
+                    eyes_prev = eyes if eyes != () else eyes_prev
+                    gw, gh, gc = glasses2.shape
+                    for i in range(0, gw):
+                        for j in range(0, gh):
+                            #print(glasses[i, j]) #RGBA
+                            if glasses2[i, j][3] != 0: # alpha 0
+                                roi_color[ey + i , ex + j ] = glasses2[i, j]
+            except IndexError:
+                pass
 
     # Display the resulting frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
